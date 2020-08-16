1fc0755dd8af207fbf66a50e957571fb193e810f
yw3zhou <yw3zhou@edu.uwaterloo.ca>
2020-08-16 19:13:19 -0400

    add pics

--- src/Projects/Projects.js
+++ src/Projects/Projects.js
@@ -1,35 +1,39 @@
 import React from "react";
 import MediaCard from "./TwoCards";
 import { AiFillGithub } from "react-icons/ai";
+import homepic from "../Pictures/OrderioHome.PNG";
 class About extends React.Component {
   render() {
     return (
       <div>
-        <div className="widthMarginBlock positionBlock biggerWidth black ">
-          <h3>
-            <em>Meet</em>
-          </h3>
-          <div className="lineContainer">
-            <h1 className="line">
-              <strong>Orderio</strong>
-            </h1>
-          </div>
+        <div className="widthMarginBlock positionBlock biggerWidth black Orderio">
+          <img src={homepic} alt="home" />
+          <div className="sideWidth widthMarginBlock biggerWidth black centre ">
+            <h3>
+              <em>Meet</em>
+            </h3>
+            <div className="lineContainer">
+              <h1 className="line">
+                <strong>Orderio</strong>
+              </h1>
+            </div>
 
-          <h4>
-            Making restaurant ordering efficient and safe. Simply scan a QR code
-            on the table, and order directly on your own phone!{" "}
-          </h4>
-          <p>
-            Amidst the pandemic, social distancing is key to containing the
-            spread. This means customers are wary of close interactions with
-            staff and servers must walk great distances from table to table.
-            Orderio is here to provide a solution.
-          </p>
-          <h4>
-            After pitching project Orderio, SalesPlus — a local POS software
-            startup with over 20 restaurant clients — has decided to partner up,
-            and introduce Orderio as the newest feature to their clients.
-          </h4>
+            <h4>
+              Making restaurant ordering efficient and safe. Simply scan a QR
+              code on the table, and order directly on your own phone!{" "}
+            </h4>
+            <p>
+              Amidst the pandemic, social distancing is key to containing the
+              spread. This means customers are wary of close interactions with
+              staff and servers must walk great distances from table to table.
+              Orderio is here to provide a solution.
+            </p>
+            <h4>
+              After pitching project Orderio, SalesPlus — a local POS software
+              startup with over 20 restaurant clients — has decided to partner
+              up, and introduce Orderio as the newest feature to their clients.
+            </h4>
+          </div>
         </div>
 
         <div className="flexContainer flexCenter twoCards flexStretch">
